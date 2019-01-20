8d4f385d4480d45b9c6ec647d60484e52ea0edbb
Monkey <dungtd@kis-v-.com>
2019-01-20 14:44:37 +0700

    update home

--- src/AppPortal.WebSite/wwwroot/js/home/home.js
+++ src/AppPortal.WebSite/wwwroot/js/home/home.js
@@ -1,5 +1,6 @@
 ﻿google.charts.load('current', { packages: ['corechart', 'bar'] });
-google.charts.setOnLoadCallback(drawMaterial);
+//google.charts.setOnLoadCallback(drawMaterial);
+google.charts.setOnLoadCallback(drawAxisTickColors);
 
 function drawMaterial() {
     var data = google.visualization.arrayToDataTable([
@@ -18,6 +19,51 @@ function drawMaterial() {
         document.getElementById('chart_div'));
     chart.draw(data, options);
 }
-    
 
+function vechart2(lstId) {
+    var dataGoogle = google.visualization.arrayToDataTable(lstId);
+    var options = {
+        title: 'Biểu đồ giá trị AQI của các trạm',
+        height: 450,
+        bar: { groupWidth: '65%' },
+        legend: {
+            alignment: 'center',
+            position: 'top'
+        },
+        annotations: {
+            textStyle: {
+                fontSize: 15,
+            },
+            alwaysOutside: true
+        }
+    };
 
+    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+    chart.draw(dataGoogle, options);
+}
+
+function drawAxisTickColors() {
+    $.ajax({
+        method: "GET",
+        url: "http://202.60.104.121/eos/services/call/json/get_stations?qi_type=aqi",
+        success: function (data, status) {
+            if (data.success == true) {
+                var dulieu = data.stations;
+                $("#listTram").html("");
+                var idFirst = 0;
+                var lstId = [['Trạm', 'Giá trị AQI', { role: 'annotation' }]];
+                for (var i = 0; i < dulieu.length; i++) {
+                    if (dulieu[i].qi > 0) {
+                        lstId.push([dulieu[i].station_name, Math.round(dulieu[i].qi), Math.round(dulieu[i].qi)]);
+                    }
+                }
+                vechart2(lstId);
+            } else {
+                alert("Lỗi API. Xin vui lòng kiểm tra lại!");
+            }
+        },
+        error: function () {
+            alert("Lỗi API. Xin vui lòng kiểm tra lại!");
+        }
+    });
+}
