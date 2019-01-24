7084f31d5fe6610795a41e0edec33889daa91bfe
kazypos <kazypos@gmail.com>
2019-01-24 18:39:07 +0700

    Search văn bản

--- src/AppPortal.AdminSite.Services/Interfaces/IMediaService.cs
+++ src/AppPortal.AdminSite.Services/Interfaces/IMediaService.cs
@@ -15,7 +15,7 @@ namespace AppPortal.AdminSite.Services.Interfaces
         Config AddOrEditConfig(string type, string url);
 
         Vanban AddOrEditVanban(Vanban model);
-        IList<Vanban> GetVanban(string type);
+        IList<Vanban> GetVanban(string type, string searchValue = "");
         void deleteVanban(int id);
     }
 }
